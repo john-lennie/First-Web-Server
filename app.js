99ddec47ae9722f9204a4ce57a1960159c05a2b6
John <johnanthonylennie@gmail.com>
2017-06-06 22:50:59 +0000

    added app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const http = require("http");
+const PORT = 8080;
+
+// a function which handles requests and sends response
+function requestHandler(request, response) {
+  if (request.url == "/") {
+    response.end("Welcome!");
+  } else if (request.url == "/urls") {
+    response.end("www.lighthouselabs.ca\nwww.google.com");
+  } else {
+    response.statusCode = 404;
+    response.end("Unknown Path");
+  }
+}
